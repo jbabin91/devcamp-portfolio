e57e0a2bff717b20f1c2ffe7e682e5b98d05117b
Jace Babin <jbabin91@gmail.com>
2017-07-11 23:45:32 -0700

    Added new and edit buttons for portfolio iteams

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class PortfoliosController < ApplicationController
   def index
-    @portfolio_item = Portfolio.all
+    @portfolio_items = Portfolio.all
   end
 
   def new
@@ -28,7 +28,9 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
-  def update
+  def update 
+    @portfolio_item = Portfolio.find(params[:id])
+    
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
